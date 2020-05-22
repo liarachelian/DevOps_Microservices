64cff94a5184bb88445603b344a63de1d3e44f82
Liarachelian <maliny1240@gmail.com>
2020-05-22 01:56:41 +0000

    add docker_out.txt

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,11 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=liarachelian/app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+Push image to a docker repository
